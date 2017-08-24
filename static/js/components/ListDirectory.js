68f3dd5aae33c432fbcc4f8a2f26afac5c86866e
ISABELLE <isabelle.k.miller@gmail.com>
2017-08-23 22:43:10 -0400

    created new list directory component

---
+++ static/js/components/ListDirectory.js
@@ -0,0 +1,37 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+class ListDirectory extends Component {
+
+  constructor(props) {
+    this.state = {
+      lists: [],
+    }
+  }
+
+  _createNewList = (event) => {
+
+  }
+
+  _cancelCreateNewList = (event) => {
+
+  }
+
+  _deleteList = (event) => {
+
+  }
+
+  _onClickDisplayList = (event) => {
+    
+  }
+
+  render = () => {
+    return(
+
+    )
+  }
+}
+
+export default ListDirectory
